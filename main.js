72d4102176df3e1b5728dc41d25bb178cb8de105
Paulina Moszczyńska <p.moszczynska@gmail.com>
2020-05-10 18:04:55 +0200

    reorganising content, adding BEM class naming convention, styles cleaning up, adding calc for page height

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const name = 'Paulina';
+const name = "Paulina";
 const age = 29;
 
 console.log(name);
-console.log(`Cześć! Nazywam się ${name} i mam ${age} lat.`)
+console.log(`Cześć! Nazywam się ${name} i mam ${age} lat.`);
 
-alert('Siema! To jest bardzo wkurzający modal - nie używaj go w ten sposób ;)')
+// alert("Siema! To jest bardzo wkurzający modal - nie używaj go w ten sposób ;)");
