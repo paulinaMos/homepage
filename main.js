a4fdf8e3f27e0e77807cbdc57f78d442efdcfa43
Paulina Moszczyńska <p.moszczynska@gmail.com>
2020-05-12 00:23:37 +0200

    adding JS .innerHTML

--- main.js
+++ main.js
@@ -5,3 +5,14 @@ console.log(name);
 console.log(`Cześć! Nazywam się ${name} i mam ${age} lat.`);
 
 // alert("Siema! To jest bardzo wkurzający modal - nie używaj go w ten sposób ;)");
+
+const header = document.querySelector('.span__js');
+
+console.log(header);
+
+header.innerHTML = 'Olaboga!';
+
+const empty = document.querySelector('.empty-element__js');
+
+empty.innerHTML = '<span class="pink">Już nie taki pusty element.</span>';
+
